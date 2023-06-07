06397164aa119d9ef8be39232cb4881da452c1d2
Joyce Lopes Krüger <homeofficina@gmail.com>
2023-06-07 19:53:59 -0300

    resolcido exercicio

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
 import { Garagem } from './Componentes/Garagem'
 import { GlobalStyled } from './GlobalStyled'
+import { useState } from 'react'
 
 export default function App() {
+  const [nome, setNome] = useState("Joyce");
+  const [automovel, setAutomovel] = useState({
+    modelo: "Fusca",
+    cor: "branco",
+    ano: 2020,
+    flex: true,
+    adicionadoPor: "Joyce"
+  })
+
+  const [car, setCar] = useState({ modelo: "C3",
+    cor: "preto",
+    ano: 2012,
+    flex: true,
+    adicionadoPor: "Joyce"
+  })
+
+  const [gol, setGol] = useState({
+    modelo: "Gol",
+    cor: "verde",
+    ano: 2015,
+    flex: true,
+    adicionadoPor: "Joyce"
+  })
+  
+  const [celta, setCelta] = useState({
+    modelo: "Celta",
+    cor: "vermelho",
+    ano: 2022,
+    flex: true,
+    adicionadoPor: "Joyce"
+  })
+
+  
   return (
     <div className="App">
       <GlobalStyled />
 
-      <Garagem nome="Labenu" />
+      <Garagem 
+      nome={nome} 
+      setNome={setNome} 
+      carro={automovel} 
+      setAutomovel={setAutomovel} 
+      carro1 = {car}
+      carro2 = {gol}
+      carro3 = {celta}
+      />
     </div>
   )
 }
--- src/Componentes/Garagem/index.js
+++ src/Componentes/Garagem/index.js
@@ -1,21 +1,43 @@
 import { Carro } from '../Carro'
 import { Botao, Estacionamento, GaragemContainer } from './styles'
 
-export function Garagem({ nome }) {
+export function Garagem({
+  nome, 
+  carro, 
+  setNome, 
+  setAutomovel,
+  carro1,
+  carro2,
+  carro3
+}) {
+  function mudaNome(){
+    if(nome === "Joyce"){
+      setNome("Luiz")
+    } else {
+      setNome("Henrique")
+    }
+  }
+  function mudaCarro(novoCarro){
+    setAutomovel (novoCarro)
+  }
   return (
     <GaragemContainer>
-      <h1>Garagem da {nome}</h1>
-      <Botao>Muda nome</Botao>
+      <h1>Garagem de {nome}</h1>
+      <Botao onClick={mudaNome}>Muda nome</Botao>
+      <Botao onClick={()=>mudaCarro(carro1)}>Muda mudaCarro 1</Botao>
+      <Botao onClick={()=>mudaCarro(carro2)}>Muda mudaCarro 2</Botao>
+      <Botao onClick={()=>mudaCarro(carro3)}>Muda mudaCarro 3</Botao>
 
       <Estacionamento>
         <Carro
-          modelo="Corsa"
-          cor="branco"
-          ano={2020}
-          adicionadoPor="Labenu"
-          flex
+          modelo={carro.modelo}
+          cor={carro.cor}
+          ano={carro.ano}
+          adicionadoPor={carro.adicionadoPor}
+          flex={carro.flex}
         />
       </Estacionamento>
     </GaragemContainer>
   )
 }
+
